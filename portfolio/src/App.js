27f1cb548f0321d2b66397b1b340de753aff69a6
Jeffuz <jeffzhang0049@gmail.com>
2023-07-31 23:53:19 -0700

    about description creation

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar';
+import About from './components/About';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <About/>
     </div>
   );
 }
