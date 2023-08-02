f17f638df765a8192c41017f552b828364cba4a4
Jeffuz <jeffzhang0049@gmail.com>
2023-08-01 19:48:47 -0700

    update about page and create contact page

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { SiDevpost, SiLinkedin, SiGithub } from "react-icons/si";
 import { Link } from 'react-router-dom'
+import { TypeAnimation } from 'react-type-animation';
 
 const About = () => {
     return (
@@ -18,21 +19,34 @@ const About = () => {
             {/* Description */}
             <div className='max-w-[800px] mt-[-96px] w-full h-screen text-left flex flex-col mx-auto justify-center'>
                 <h1 className='text-[#42C2FF] font-poppins text-6xl font-bold leading-16 mx-8'>Hello, I'm Jeff Zhang. </h1>
-                <h3 className='text-[#42C2FF] font-poppins text-2xl font-light leading-[266.667%] mx-8'>Student </h3>
-                <p className='text-[#42C2FF] font-poppins text-xl font-medium leading-[210%] mx-8'> I am a fourth year student studying Computer Engineering
+                <h3 className='text-[#42C2FF] font-poppins text-2xl font-light leading-[266.667%] mx-8'>
+                    <span>I am </span>
+                    <TypeAnimation sequence={[
+                        'a Student.',
+                        2000,
+                        'a Developer.',
+                        2000,
+                        'an Engineer.',
+                        2000,
+                    ]}
+                        speed={50}
+                        repeat={Infinity}
+                    />
+                </h3>
+                <p className='text-[#42C2FF] font-poppins text-xl font-medium leading-[210%] mx-8'> I will be a fourth year student studying Computer Engineering
                     at <span className='text-[#85F4FF]'>UC Santa Cruz</span>.
                     My passion lies in crafting web apps and software solutions from scratch. Currently seeking a Web Developer role to put my skills into action and make an impact. Let's build something amazing together!
                 </p>
                 <div className='flex items-center class'>
                     <button className='bg-[#85F4FF] hover:bg-[#42C2FF] hover:text-[#EFFFFD] duration-300 ease-in-out shadow-lg w-[200px] rounded-md font-medium my-6 px-6 py-3 mx-8' onClick={() => window.location.href = 'mailto:jeffzhang0049@gmail.com'} style={{ zIndex: 1 }}>Contact Me</button>
                     <div className='flex text-gray-400 ' style={{ zIndex: 1 }}>
-                        <Link to="https://github.com/Jeffuz">
+                        <Link to="https://github.com/Jeffuz" className='icon-link'>
                             <SiGithub size={35} />
                         </Link>
-                        <Link to="https://www.linkedin.com/in/jeffzhang0049/">
+                        <Link to="https://www.linkedin.com/in/jeffzhang0049/" className='icon-link'>
                             <SiLinkedin size={35} className='ml-5' />
                         </Link>
-                        <Link to="https://devpost.com/jeffzhang0049?ref_content=user-portfolio&ref_feature=portfolio&ref_medium=global-nav">
+                        <Link to="https://devpost.com/jeffzhang0049?ref_content=user-portfolio&ref_feature=portfolio&ref_medium=global-nav" className='icon-link'>
                             <SiDevpost size={35} className='ml-5' />
                         </Link>
                     </div>
--- portfolio/src/pages/contact.js
+++ portfolio/src/pages/contact.js
@@ -1,9 +1,45 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 export default function Contact() {
     return (
         <div>
-            Contact Page
+            <Navbar />
+            <section className="py-16 lg:section">
+                <div className="container mx-auto">
+                    <div className="flex flex-col lg:flex-row">
+                        {/* text */}
+                        <div className="flex-1 flex justify-start items-center">
+                            <div>
+                                <h4 className="text-xl uppercase text-accent font-medium mb-2 tracking-wide">Get in touch</h4>
+                                <h2 className="text-[45px] lg:text-[90px] leading-none mb-12">Let's work <br /> together!
+                                </h2>
+                            </div>
+                        </div>
+                        {/* form */}
+                        <form className="flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 itmes-start">
+                            <input
+                                className="bg-transparent border-b py-3 outline-none w-full placeholder:text-[#42C2FF] focus:border-accent transition-all"
+                                type='text'
+                                placeholder="Your name"
+                            >
+                            </input>
+                            <input
+                                className="bg-transparent border-b py-3 outline-none w-full placeholder:text-[#42C2FF] focus:border-accent transition-all"
+                                type='text'
+                                placeholder="Your email"
+                            >
+                            </input>
+                            <textarea
+                                className="bg-transparent border-b py-3 outline-none w-full placeholder:text-[#42C2FF] focus:border-accent transition-all resize-none mb-12"
+                                placeholder="Your message"
+                            >
+                            </textarea>
+                            <button className='bg-[#85F4FF] hover:bg-[#42C2FF] hover:text-[#EFFFFD] text-white duration-300 ease-in-out shadow-lg w-[200px] rounded-md font-medium px-6 py-3'>Send message</button>
+                        </form>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
