c99107dad76466861f9ee34f678ce0c5dc194c9c
Jeffuz <jeffzhang0049@gmail.com>
2023-10-03 11:44:16 -0700

    update experience page

--- src/components/expCard.jsx
+++ src/components/expCard.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import { FaGoogle } from "react-icons/fa6"
+import { FaGoogle, FaRobot, FaBook, FaRocket } from "react-icons/fa6"
 import { GiGraspingSlug } from "react-icons/gi"
+import { BiMath } from "react-icons/bi"
 import experiences from "../data/expData.json"
 
 const ExpCard = () => {
     const iconMap = {
         FaGoogle: <FaGoogle />,
         GiGraspingSlug: <GiGraspingSlug />,
+        FaRobot: <FaRobot />,
+        FaBook: <FaBook />,
+        BiMath: <BiMath />,
+        FaRocket: <FaRocket />
     };
 
     return (
@@ -25,12 +30,7 @@ const ExpCard = () => {
                             <div className="font-bold" >{exp.name}</div>
                             <div className="vertical-timeline-element-subtitle">{exp.location}</div>
                         </div>
-                        <div className='font-bold'>{exp.role}</div>
-                        <ul className='list-disc pl-5'>
-                            {Object.values(exp.duties).map((duty, i) => (
-                                <li key={i}>{duty}</li>
-                            ))}
-                        </ul>
+                        <div className='font-semibold italic'>{exp.role}</div>
                     </VerticalTimelineElement>
                 ))}
             </VerticalTimeline>
