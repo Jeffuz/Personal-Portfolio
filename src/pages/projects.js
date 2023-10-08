d54d6f13225d38b94019e851bdae4b03d67f51a0
Jeffuz <jeffzhang0049@gmail.com>
2023-10-08 16:02:23 -0700

    complete project page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,18 +1,17 @@
 import React from "react";
-import { motion } from "framer-motion";
+import projectData from "../data/projData.json"
+import ProjCard from "../components/projCard";
 
 export default function Projects() {
     return (
         <div>
-            <motion.p
-                animate={{ opacity: [0, 1], y: [30, 0] }}
-                exit={{ opacity: [1, 0] }}
-                transition={{ duration: 1 }}
-            >
-                <div className='flex justify-center text-[#42C2FF] mt-10 font-bold text-3xl'>
-                    <span className='mx-auto border-b-2 border-[#42C2FF] p-2'>My Personal Projects</span>
-                </div>
-            </motion.p>
+
+            <div>
+                {projectData.map((proj) => (
+                    <ProjCard key={proj.id} {...proj} />
+                ))}
+            </div>
+
         </div>
     )
 }
