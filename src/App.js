acc4ad6cad9ceb915eca3364a97a7c9c52129575
Jeffuz <jeffzhang0049@gmail.com>
2024-02-01 21:49:56 -0800

    finish me section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import React from 'react';
 import Me from './components/me';
+import About from './components/about';
+import Experience from './components/experience';
 
 function App() {
   return (
     <>
-      <div className='mx-6 mt-12 pb-24'><Me/></div>
+      <div className='app_module_format mt-12'><Me/></div>
+      <div className='app_module_format'><About/></div>
+      <div className='app_module_format'><Experience/></div>
     </>
   );
 }
--- src/components/me.jsx
+++ src/components/me.jsx
@@ -3,14 +3,13 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { IoMail } from "react-icons/io5";
 import { SiDevpost } from "react-icons/si";
 
-
 const Me = () => {
   return (
     <div className='flex flex-col gap-3 w-[60%]'>
         {/* Name */}
         <div id='user_name' className='text-4xl font-bold text-slate-200'>Jeff Zhang</div>
         {/* Current Position */}
-        <div id='user_current_position' className='text-xl font-medium text-slate-200'>Software Developer at UCSC Police Department</div>
+        <div id='user_current_position' className='text-lg font-semibold text-slate-200'>Software Developer at UCSC Police Department</div>
         {/*  Slogan */}
         <div id='user_slogan' className='text-slate-400 text-md font-medium'>My passion lies in crafting web apps and software solutions from scratch.</div>
         {/* Social Media Icons */}
