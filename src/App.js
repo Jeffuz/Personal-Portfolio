c66136b5fc090fc08392d3c1336d7923fa765de7
Jeff Zhang <52511888+Jeffuz@users.noreply.github.com>
2024-07-23 17:39:39 +0800

    use google analytics

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import './App.css';
-import React from 'react';
-import Navbar from './components/navbar';
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
+import "./App.css";
+import React, { useEffect } from "react";
+import Navbar from "./components/navbar";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Home from "./pages/home";
+import ReactGA from "react-ga";
+
+const TRACKING_ID = "G-0GK5L2X0C6";
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.initialize(TRACKING_ID);
+  }, []);
+
+  useEffect(() => {
+    ReactGA.set({ page: location.pathname });
+    ReactGA.pageview(location.pathname);
+  }, [location]);
+
   return (
     <>
-      <HashRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />}/>
-        </Routes>
-      </HashRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { HashRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <HashRouter>
+    <App />
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
