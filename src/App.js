20beb7205fbd56760c8371ab5fb6229960b2351b
Jeff Zhang <52511888+Jeffuz@users.noreply.github.com>
2024-07-23 17:51:55 +0800

    Revert "use google analytics"
    This reverts commit c66136b5fc090fc08392d3c1336d7923fa765de7.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,18 @@
-import "./App.css";
-import React, { useEffect } from "react";
-import Navbar from "./components/navbar";
-import { Routes, Route, useLocation } from "react-router-dom";
-import Home from "./pages/home";
-import ReactGA from "react-ga";
-
-const TRACKING_ID = "G-0GK5L2X0C6";
+import './App.css';
+import React from 'react';
+import Navbar from './components/navbar';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
 
 function App() {
-  const location = useLocation();
-
-  useEffect(() => {
-    ReactGA.initialize(TRACKING_ID);
-  }, []);
-
-  useEffect(() => {
-    ReactGA.set({ page: location.pathname });
-    ReactGA.pageview(location.pathname);
-  }, [location]);
-
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <HashRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+        </Routes>
+      </HashRouter>
     </>
   );
 }
