bb8864456c25ce5b5c23c66c1d5bc1479899ab2e
Jeff Zhang <52511888+Jeffuz@users.noreply.github.com>
2024-07-26 09:51:22 +0800

    update about me

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -16,7 +16,11 @@ const About = () => {
                     Recently, I was involved in leading a project and developing an application for the
                     <span className='about_bold_importance'> <a href='https://police.ucsc.edu/' target='_blank'>UCSC Police Department</a></span>.
                     I take pleasure in creating products that not only meet but surpass the expectations of customers and users.
-
+                    Additionally, I have explored AI engineering by developing applications like 
+                    <span className='about_bold_importance'> <a href='https://github.com/Jeffuz/Eyes2Ears' target='_blank'>Eyes2Ears</a></span>
+                    , a mobile app that assists visually impaired individuals using advanced AI technologies, and 
+                    <span className='about_bold_importance'> <a href='https://github.com/Jeffuz/EcoScan' target='_blank'>EcoScan</a></span>
+                    , which assesses the environmental impact of products with AI-driven insights.
                     Outside of work, I participate in
                     <span className='about_bold_importance'> <a href='https://devpost.com/hackathons' target='_blank'>hackathons</a> </span> to explore new ideas and connect with like-minded individuals.
                 </div>
